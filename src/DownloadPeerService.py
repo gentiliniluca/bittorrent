81000e78305ed99e36f3192e6e2648a01d61cf36
lorenzomodenese <lorenzo.modenese@student.unife.it>
2014-05-19 21:32:54 +0200

    modifiche e metodo getPeerfromIp

--- src/DownloadPeerService.py
+++ src/DownloadPeerService.py
@@ -19,6 +19,16 @@ class DownloadPeerService:
         return downloadPeer
     
     @staticmethod
+    def getPeerfromIp(database, ipp2p):
+        database.execute("""SELECT downloadpeerid, ipp2p, pp2p
+                            FROM DownloadPeer
+                            WHERE ipp2p = %s""",
+                            ipp2p)
+        downloadpeerid, ipp2p, pp2p = database.fetchone()
+        downloadPeer = DownalodPeer.DownloadPeer(dowanladpeerid, ipp2p, pp2p)
+        return downloadPeer
+    
+    @staticmethod
     def getDownloadPeer(database, downloadpartid):
         database.execute("""SELECT downloadpeerid, ipp2p, pp2p
                             FROM DownloadPeer, DownloadPart
